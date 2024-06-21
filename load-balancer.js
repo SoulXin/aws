68e6cc0b977fe1407840dfe215a0bc9bbf3db95c
SoulXin <xin@xins-MacBook-Pro.local>
2024-06-21 21:37:37 +0700

    add load-balancer

---
+++ load-balancer.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let serverStatus = true;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/check-health', (req, res) => {
+  if (serverStatus) {
+    res.send('Server is good');
+  } else {
+    res.send('Server is down');
+  }
+});
+
+app.get('/shutdown', (req, res) => {
+  serverStatus = false;
+  res.send('Server status changed to down');
+});
+
+app.get('/start', (req, res) => {
+  serverStatus = true;
+  res.send('Server status changed to good');
+});
+
+app.get('/random', (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  res.send(`Your lucky number is ${randomNumber}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running`);
+});
