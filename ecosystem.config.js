fcd021a5ae5778857586404b65e6894f63910db3
SoulXin <xin@xins-MacBook-Pro.local>
2024-06-21 22:46:17 +0700

    Fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  apps: [
+  apps : [
     {
-      name: 'admin',
-      exec_mode: 'cluster',
+      name: 'client',
+      script: './index.js',
       instances: 1,
-      script: 'node_modules/next/dist/bin/next',
       args: 'start',
       autorestart: true,
       watch: false,
       max_memory_restart: '150M',
       env: {
-        PORT: 3002
+        PORT: 3001
       }
     }
   ]
-}
+};
